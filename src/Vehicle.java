e72d91aebf38c6035e93296b2310e04894a6b4e5
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-06-13 18:18:13 -0300

    definindo uma classe

---
+++ src/Vehicle.java
@@ -0,0 +1,6 @@
+public class Vehicle {
+
+    int passengers;
+    int fuelcap;
+    int mpg;
+}
---
+++ src/VehicleDemo.java
@@ -0,0 +1,29 @@
+public class VehicleDemo {
+
+    public static void main(String[] args) {
+
+        //minivan é uma instância da classe Vehicle, portanto, tem realidade "física"
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+
+        int range1, range2;
+
+        //o operador ponto vincula o nome de um objeto ao nome de um membro, podendo ser usado para acessar
+        //variáveis de intância ou métodos.
+        minivan.passengers = 6;
+        minivan.fuelcap = 1;
+        minivan.mpg = 22;
+
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 3;
+        sportscar.mpg = 12;
+
+        range1 = minivan.fuelcap * minivan.mpg;
+        range2 = minivan.fuelcap * minivan.mpg;
+
+        System.out.println("Minivan can carry " + minivan.passengers + " with a range of " + range1);
+
+        System.out.println("Minivan can carry " + sportscar.passengers + " with a range of " + range2);
+
+    }
+}
