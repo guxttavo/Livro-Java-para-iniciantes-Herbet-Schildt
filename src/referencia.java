07c666c605952a44e9e5a7c73883a3fc6898f459
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-06-17 23:58:25 -0300

    referencias da superclasse e objetos da subclasse

---
+++ src/referencia.java
@@ -0,0 +1,16 @@
+public class referencia {
+
+    public static void main(String[] args) {
+
+    }
+
+    class x {
+        int a;
+
+        x(int i) {
+            a = i;
+        }
+    }
+
+}
+
