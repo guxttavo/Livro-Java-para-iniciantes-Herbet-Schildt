bee2cef00e6c3a24d0f1373e36aadd6e9a9be161
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-06-20 19:05:02 -0300

    arrays multidimensionais

---
+++ src/Bubble.java
@@ -0,0 +1,37 @@
+public class Bubble {
+    public static void main(String[] args) {
+        int nums[] = {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};
+
+        int a, b, t;
+        int size;
+
+        size = 10;
+
+        //exibe o array original
+        System.out.println("Original array is: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + nums[i]);
+        }
+        System.out.println();
+
+        //classificação por bolha
+        for (a = 1; a < size; a++) {
+            for (b = size - 1; b >= a; b--) {
+                //se fora de ordem
+                if (nums[b - 1] > nums[b]) {
+                    //troca elementos
+                    t = nums[b - 1];
+                    nums[b - 1] = nums[b];
+                    nums[b] = t;
+                }
+            }
+        }
+
+        //exibe o array classificado
+        System.out.print("Sorted array is: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + nums[i]);
+            System.out.println();
+        }
+    }
+}
